2ef7ab448f48cba55ec7f0adc1feba494cae05e0
Jonathan Maia <jtauan10@gmail.com>
2023-05-18 14:15:58 -0300

    questao 02 letra d #2

---
+++ tarefas/tarefa02/q02d.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION ObterDiasAtrasoProjeto(codigo_ativ INT)
+RETURNS INT
+LANGUAGE plpgsql
+AS
+$$
+DECLARE
+	dias INTEGER;
+BEGIN
+	SELECT datafim - DATE(CURRENT_DATE AT TIME ZONE 'America/Fortaleza')
+	INTO dias
+	FROM atividade
+	WHERE codigo = codigo_ativ;
+	
+	RETURN dias;
+END;
+$$;
